a20dddf1ba25b20fc43370a9ac9275e7961d5f90
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-11-15 17:23:52 +0530

    [Kartikeya] UC added to calculate emp wage with use of function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -17,7 +17,7 @@ const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
-let empHrs=0;
+empHrs=0;
 empCheck=Math.floor(Math.random()*10)%3;
 switch (empCheck)
 {
@@ -32,3 +32,21 @@ switch (empCheck)
 }
 let empWage=empHrs*WAGE_PER_HOUR;
 console.log("Employee wage: "+empWage);
+
+// UC 3
+function GetWorkingHours(empCheck)
+{
+    switch(empCheck)
+    {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+            break;
+        default:
+            return 0;
+    }
+}
+empHrs=GetWorkingHours(empCheck);
+console.log("Employee Wage: "+empWage);
